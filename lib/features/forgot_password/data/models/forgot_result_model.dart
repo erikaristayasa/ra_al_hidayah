8f0dfe5b815586ee4916aaf2f72a9dec31ab86d5
erikappkey <erik.appkey@gmail.com>
2022-07-15 20:53:27 +0800

    show otp for test

--- lib/features/forgot_password/data/models/forgot_result_model.dart
+++ lib/features/forgot_password/data/models/forgot_result_model.dart
@@ -2,13 +2,15 @@ import 'package:equatable/equatable.dart';
 
 class ForgotResultModel extends Equatable {
   final String token;
+  final dynamic code;
 
-  const ForgotResultModel({required this.token});
+  const ForgotResultModel({required this.token, required this.code});
 
   @override
-  List<Object?> get props => [token];
+  List<Object?> get props => [token, code];
 
   factory ForgotResultModel.fromJson(Map<String, dynamic> json) => ForgotResultModel(
         token: json['token'],
+        code: json['user'] != null ? json['user']['otp'] : ''
       );
 }
--- lib/features/forgot_password/data/repositories/forgot_password_repository_implementation.dart
+++ lib/features/forgot_password/data/repositories/forgot_password_repository_implementation.dart
@@ -1,4 +1,6 @@
 import 'package:dartz/dartz.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:ra_al_hidayah/core/utilities/utilities.dart';
 
 import '../../../../core/errors/failure.dart';
 import '../../../../core/networks/networks.dart';
@@ -20,11 +22,13 @@ class ForgotPasswordRepositoryImplementation implements ForgotPasswordRepository
       try {
         final result = await dataSource.forgotPassword(phone);
         if (result.message == 'OK') {
+          Fluttertoast.showToast(msg: '(for test) code: ${result.data.code}', toastLength: Toast.LENGTH_LONG);
           return Right(result.data.token);
         } else {
           return Left(ServerFailure(message: result.message));
         }
       } catch (e) {
+        AppHelpers.logMe(e.toString());
         return const Left(ServerFailure());
       }
     } else {
